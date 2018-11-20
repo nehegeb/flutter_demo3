0018ad4bb4708f742ba8dcd1b3b654fb7bbaa12e
nehegeb <git@nehegeb.de>
2018-11-21 00:30:10 +0100

    step5 text input box
    Added a text input box. When submitting the message, it'll just get deleted by now.

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,36 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text('Demo3 | Friendlychat')),
+
+      body: _buildTextComposer(),
     );
   }
+
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: new InputDecoration.collapsed(hintText: 'Write some stuff'),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
 }
