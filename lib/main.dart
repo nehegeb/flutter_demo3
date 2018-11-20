527951ab9aae95f4296a75344188db5aa9b7353e
nehegeb <git@nehegeb.de>
2018-11-20 23:55:06 +0100

    Step4 main user interface
    The main user interface has been implemented. It didn't change the visible app, but introduced stateless classes.
    Also I painted the appbar black.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    new MaterialApp(
-      title: "Flutter Demo3",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Demo3 | Friendlychat"),
-        ),
+  runApp(new FriendlychatApp());
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo3',
+      theme: ThemeData(
+        primaryColor: Colors.black,
       ),
-    ),
-  );
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Demo3 | Friendlychat')),
+    );
+  }
 }
