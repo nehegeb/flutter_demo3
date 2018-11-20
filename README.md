2e6614f403493c7093fc80782641201b10b7fdfc
nehegeb <git@nehegeb.de>
2018-11-20 23:32:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter_demo3
+
+Learning Flutter by doing the "getting started" demos of the flutter.io website.
+
+## Chat app with beautiful UI
+
+[This Demo](https://codelabs.developers.google.com/codelabs/flutter/#2)
+creates a nice looking chat app.
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: "Flutter Demo3",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Demo3 | Friendlychat"),
+        ),
+      ),
+    ),
+  );
+}
