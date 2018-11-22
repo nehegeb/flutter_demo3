882d2acd19705900372ef420cf3a2c74f3d40f12
nehegeb <git@nehegeb.de>
2018-11-22 21:02:00 +0100

    step6 chat message listing
    Added the actual chat messages within a scrolling list. Now you can enter some text to be displayed in that list.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+/*
+SETTINGS
+*/
+
+const String _name = 'Nob Ody';
+
+/*
+MAIN APP
+*/
+
 void main() {
   runApp(new FriendlychatApp());
 }
@@ -18,20 +28,49 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
+/*
+CHAT ROUTE
+*/
+
 class ChatScreen extends StatefulWidget {
   @override
   State createState() => new ChatScreenState();
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text('Demo3 | Friendlychat')),
+      body: new Column(
+        children: <Widget>[
 
-      body: _buildTextComposer(),
+          // List of chat messages.
+          new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+
+          // A divider between the chat messages and the input box.
+          new Divider(height: 1.0),
+
+          // The message input box.
+          new Container(
+            decoration: new BoxDecoration(
+              color: Theme.of(context).cardColor
+            ),
+            child: _buildTextComposer(),
+          ),
+
+        ],
+      ),
     );
   }
 
@@ -69,6 +108,53 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+}
+
+// The chat message box itself.
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+
+          // Avatar icon.
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0])),
+          ),
+
+          // Message box.
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              )
+
+            ],
+          ),
+
+        ],
+      ),
+    );
   }
 
 }
