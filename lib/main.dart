40ab3b3d0ef7e04fb022199e11cab2572afac648
nehegeb <git@nehegeb.de>
2018-11-22 23:51:40 +0100

    step8 distinguishing platforms
    Added different designs according to the current platform being iOS or Android(default).
    (Couldn't test the iOS stuff...)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 
 /*
 SETTINGS
 */
 
 const String _name = 'Nob Ody';
+
+/*
+THEME DATA
+*/
+
+// iOS theme.
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+// Default and Android theme.
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.green,
+  accentColor: Colors.lime,
+);
 
 /*
 MAIN APP
@@ -19,10 +38,9 @@ class FriendlychatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo3',
-      theme: ThemeData(
-        primaryColor: Colors.orange,
-        accentColor: Colors.orange,
-      ),
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+        ? kIOSTheme
+        : kDefaultTheme,
       home: new ChatScreen(),
     );
   }
@@ -45,33 +63,47 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text('Demo3 | Friendlychat')),
-      body: new Column(
-        children: <Widget>[
+      appBar: new AppBar(
+        title: new Text('Demo3 | Friendlychat'),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
 
-          // List of chat messages.
-          new Flexible(
-            child: new ListView.builder(
-              padding: new EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
+            // List of chat messages.
+            new Flexible(
+              child: new ListView.builder(
+                padding: new EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              ),
             ),
-          ),
 
-          // A divider between the chat messages and the input box.
-          new Divider(height: 1.0),
+            // A divider between the chat messages and the input box.
+            new Divider(height: 1.0),
 
-          // The message input box.
-          new Container(
-            decoration: new BoxDecoration(
-              color: Theme.of(context).cardColor
+            // The message input box.
+            new Container(
+              decoration: new BoxDecoration(
+                color: Theme.of(context).cardColor
+              ),
+              child: _buildTextComposer(),
             ),
-            child: _buildTextComposer(),
-          ),
 
-        ],
-      ),
+          ],
+        ),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+          ? new BoxDecoration(
+            border: new Border(
+              top: new BorderSide(
+                color: Colors.grey[200]
+              ),
+            ),
+          )
+          : null
+        ),
     );
   }
 
@@ -100,13 +132,20 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
             // Send text button.
             new Container(
               margin: new EdgeInsets.symmetric(horizontal: 4.0),
-              child: new IconButton(
-                icon: new Icon(Icons.send),
-                onPressed: _isComposing
-                  ? () => _handleSubmitted(_textController.text)
-                  : null,
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                ? new CupertinoButton(
+                  child: new Text('Send'),
+                  onPressed: _isComposing
+                    ? () => _handleSubmitted(_textController.text)
+                    : null,
+                  )
+                : new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: _isComposing
+                    ? () => _handleSubmitted(_textController.text)
+                    : null,
+                ),
               ),
-            ),
 
           ],
         ),
@@ -184,7 +223,7 @@ class ChatMessage extends StatelessWidget {
                     margin: const EdgeInsets.only(top: 5.0),
                     child: new Text(text),
                   )
-                  
+
                 ],
               ),
             ),
